de26a52928b708d0237210e08360869f4eed331a
Miach-A <ovmiach@gmail.com>
2022-10-07 12:49:57 +0300

    init

---
+++ MessengerModel/DeletedMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerModel
+{
+    public class DeletedMessage
+    {
+        public Guid Guid { get; set; }
+        public Chat Chat { get; set; } = null!;
+        public Guid ChatGuid { get; set; }
+        public User User { get; set; } = null!;
+        public Guid UserGuid { get; set; }
+        public Message Message { get; set; } = null!;
+        public Guid MessageGuid { get; set; }
+
+    }
+}
---
+++ MessengerModel/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerModel
+{
+    public class Message
+    {
+        public Guid Guid { get; set; }
+        public DateTime Date { get; set; }
+        public Chat Chat { get; set; } = null!;
+        public Guid ChatGuid { get; set; }
+        public User User { get; set; }
+        public Guid UserGuid { get; set; }
+        public string Text { get; set; } = String.Empty;
+    }
+}
