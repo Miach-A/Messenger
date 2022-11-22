8b7e23dbf1440b06b019e0fdccbd97dd8592e3b8
Miach-A <ovmiach@gmail.com>
2022-11-22 11:55:26 +0200

    save

--- MessengerData/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MessengerData/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -20,7 +20,7 @@ namespace MessengerData.Migrations
                 .HasAnnotation("ProductVersion", "6.0.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
             modelBuilder.Entity("MessengerModel.ChatModelds.Chat", b =>
                 {
